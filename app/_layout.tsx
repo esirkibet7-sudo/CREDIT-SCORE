7e42dcd1b9d4f4e8d1cdd164de162900739e01f1
SEA WEED <esirkibet7@gmail.com>
2026-09-21 18:23:59 +0300

    Remove broken onboarding splash flow

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -17,7 +17,7 @@ import 'react-native-reanimated';
 import { useColorScheme } from '@/hooks/use-color-scheme';
 
 export const unstable_settings = {
-  initialRouteName: 'index',
+  initialRouteName: 'auth',
 };
 
 function OfflineModal() {
